0c131f223dace3a031503fa40cb4d1a45f698635
Ayoub Assairh <ayoub.assairh@labom2iformation.fr>
2024-06-10 12:07:11 +0200

    Add Exercice 4 & 5

--- 04_arrow.js
+++ 04_arrow.js
@@ -1,23 +1,25 @@
-
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
  * Le return est explicite
- * 
- * contrainte: 
+ *
+ * contrainte:
  *   - le mot clée "function" est interdis
  */
-
-let arrow1 = () => {}
+let arrow1 = x => {
+    return x + 1
+}
 
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
  * Le return est implicite
- * 
- * contrainte: 
+ *
+ * contrainte:
  *   - le mot clée "function" est interdis
  *   - le mot clée "return" est interdis
  */
+let arrow2 = x => x + 1
 
-let arrow2 = () => {}
+console.log(arrow1(1))
+console.log(arrow2(1))
 
-module.exports = {arrow1, arrow2};
+module.exports = { arrow1, arrow2 }
